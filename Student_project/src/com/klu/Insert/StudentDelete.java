f6d517b2164b0ffae8d2e832811677ff9eb0a17c
MICHANAGATLADINESH778 <74001365+MICHANAGATLADINESH778@users.noreply.github.com>
2021-01-22 16:45:46 +0530

    Add files via upload

---
+++ Student_project/src/com/klu/Insert/StudentDelete.java
@@ -0,0 +1,20 @@
+package com.klu.Insert;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.klu.Bean.StudentBean;
+import com.klu.Connections.DBUtil;
+
+public class StudentDelete {
+
+	public int StudentDelete(StudentBean student) throws ClassNotFoundException,SQLException{
+		Connection con2=DBUtil.DBConnection();
+		PreparedStatement ps_delete=con2.prepareStatement("delete from Student where regno=?");
+		ps_delete.setInt(1,student.getRegno());
+		int i=ps_delete.executeUpdate();
+		con2.close();
+		return i;
+		}
+
+}
